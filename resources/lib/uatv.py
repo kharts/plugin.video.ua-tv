583194759df280c1e840705fbbcfda798f1d07f6
kharts <kharts@ukr.net>
2015-09-21 14:22:21 -0700

    Add docstring to uatv.py. Add index() and addChannel() declaration

--- resources/lib/uatv.py
+++ resources/lib/uatv.py
@@ -1,8 +1,36 @@
-# uatv.py - functions for watching Ukrainian TV Channels.
+# uatv.py - with functions for watching Ukrainian TV Channels.
 #
 # Copyright 2015 kharts (https://github.com/kharts)
 #
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
+"""
+uatv.py - module with functions for watching Ukrainian TV Channels.
+Public functions:
+index() - opens main menu of the plugin.
+
+addChannel(name) - adds channel to the main menu of the plugin.
+"""
+
 __author__ = 'kharts'
+
+
+def index():
+    """
+    Opens main manu of the plugin.
+    :return: None
+    """
+
+    pass
+
+
+def addChannel(name):
+    """
+    Adds channel to the main menu of the plugin
+    :param name: - name of channel
+    :return: None
+    """
+
+    pass
+
