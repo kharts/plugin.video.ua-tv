02404f1f34546bc6add000e01f5e82e2719abcdc
kharts <kharts@ukr.net>
2015-09-21 14:03:21 -0700

    Add uatv.py

---
+++ resources/lib/uatv.py
@@ -0,0 +1,8 @@
+# uatv.py - functions for watching Ukrainian TV Channels.
+#
+# Copyright 2015 kharts (https://github.com/kharts)
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+__author__ = 'kharts'
