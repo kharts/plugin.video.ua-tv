2c8b6affc1ec329b4e7c3af8bd2b257a086c2906
kharts <kharts@ukr.net>
2015-09-21 14:05:05 -0700

    Add copyrigth notice to default.py

--- default.py
+++ default.py
@@ -1,1 +1,8 @@
+# default.py - main module.
+#
+# Copyright 2015 kharts (https://github.com/kharts)
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version
+
 __author__ = 'kharts'
